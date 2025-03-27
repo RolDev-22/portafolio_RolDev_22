2ae052cb2bc0a0bb58a46ae4674c584f8ab8b588
RolDev-22 <rolandomuagui1629@gmail.com>
2025-03-26 22:55:44 -0600

    cambios en rutas de imagenes

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,6 @@ const imgIcon = document.querySelector(".iconImg");
 
 menu.addEventListener("click", () => {
   imgIcon.src.includes("sun.svg")
-    ? (imgIcon.src = "../assents/moon.svg")
-    : (imgIcon.src = "../assents/sun.svg");
+    ? (imgIcon.src = "assents/moon.svg")
+    : (imgIcon.src = "assents/sun.svg");
 });
