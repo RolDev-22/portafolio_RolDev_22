b4039b344bc524a6aa9af59ae9b911a7e2eecaa8
RolDev-22 <rolandomuagui1629@gmail.com>
2025-03-26 23:02:10 -0600

    cambios

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const menu = document.querySelector(".icon");
 const imgIcon = document.querySelector(".iconImg");
 
 menu.addEventListener("click", () => {
-  imgIcon.src.includes("sun.svg")
-    ? (imgIcon.src = "assents/moon.svg")
-    : (imgIcon.src = "assents/sun.svg");
+  imgIcon.src.includes("assets/sun.svg")
+    ? (imgIcon.src = "assets/moon.svg")
+    : (imgIcon.src = "assets/sun.svg");
 });
