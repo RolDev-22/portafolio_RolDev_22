cda55fda908cbfc31c38cc428b2e1ed58aa0bcd4
RolDev-22 <rolandomuagui1629@gmail.com>
2025-06-11 19:46:26 -0600

    restructuración

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".icon");
-const imgIcon = document.querySelector(".iconImg");
-
-menu.addEventListener("click", () => {
-  imgIcon.src.includes("assets/sun.svg")
-    ? (imgIcon.src = "assets/moon.svg")
-    : (imgIcon.src = "assets/sun.svg");
-});
