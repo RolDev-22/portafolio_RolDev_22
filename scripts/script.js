8e487e5bc5261bb3ea33a294dbeb20252e5fd23c
RolDev-22 <rolandomuagui1629@gmail.com>
2025-06-23 21:36:03 -0600

    Cambios en modularidad de js, personalizacion de primera seccion y diseño

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,23 @@ const nav = document.querySelector("nav");
 const mode = document.getElementById("mode");
 const header = document.querySelector("header");
 const option = document.querySelectorAll(".option-Menu");
-
 let modeState = document.body.classList.contains("dark");
 
+/* Eventos */
+/* Estos eventos sirven para abrir y cerrar el menu recorren la lista de opciones
+  y cuando se hace click en una de ellas se llama a la funcion navToggle */
 option.forEach((option) => {
   option.addEventListener("click", () => {
     navToggle();
   });
 });
 
+/* Este evento sirve para cambiar el modo oscuro, icono y color de fondo */
 mode.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   header.classList.toggle("darNav");
   nav.classList.toggle("darNav");
-  imageMode(!modeState);
+  imageMode(modeState);
   navToggle();
 });
 
@@ -28,6 +31,6 @@ function navToggle() {
 
 /* Esta funcion sirve para cambiar el icono del modo oscuro */
 function imageMode(condition) {
-  mode.src = condition ? "assets/sun.svg" : "assets/moon.svg";
+  mode.src = condition ? "assets/moon.svg" : "assets/sun.svg";
   modeState = !modeState;
 }
