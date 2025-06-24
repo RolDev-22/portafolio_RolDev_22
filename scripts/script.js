3b5db8540df3d5f964a90fb7697390d174002a26
RolDev-22 <rolandomuagui1629@gmail.com>
2025-06-23 20:19:39 -0600

    Cambio en estilos y agregación de funcionalidades de menu y modo, además, se crean secciones

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,33 @@
-const menu = document.getElementById("menu");
 const nav = document.querySelector("nav");
 const mode = document.getElementById("mode");
 const header = document.querySelector("header");
+const option = document.querySelectorAll(".option-Menu");
 
-menu.addEventListener("click", () => {
-  nav.classList.toggle("active");
+let modeState = document.body.classList.contains("dark");
+
+option.forEach((option) => {
+  option.addEventListener("click", () => {
+    navToggle();
+  });
 });
 
 mode.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-  header.classList.toggle("dark");
-  nav.classList.toggle("active");
+  header.classList.toggle("darNav");
   nav.classList.toggle("darNav");
-  mode.src = document.body.classList.contains("dark")
-    ? "assets/sun.svg"
-    : "assets/moon.svg";
+  imageMode(!modeState);
+  navToggle();
 });
+
+/* Funciones */
+
+/* Esta funcion sirve para abrir y cerrar el menu */
+function navToggle() {
+  nav.classList.toggle("active");
+}
+
+/* Esta funcion sirve para cambiar el icono del modo oscuro */
+function imageMode(condition) {
+  mode.src = condition ? "assets/sun.svg" : "assets/moon.svg";
+  modeState = !modeState;
+}
