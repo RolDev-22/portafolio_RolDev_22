3cfd94443e593551313c5c437664bf4e84103084
RolDev-22 <rolandomuagui1629@gmail.com>
2025-09-07 20:42:10 -0600

    Correccion de funciones y actualizacion de responsive

--- scripts/generally_scripts.js
+++ scripts/generally_scripts.js
@@ -8,12 +8,7 @@ btnMenu.addEventListener("click", () => {
 
 optionsMenu.forEach((option) => {
   const nav = document.querySelector("nav");
-  option.addEventListener("click", () => {
-    // Primero quitamos la clase a todos
-    optionsMenu.forEach((opt) => opt.classList.remove("activeMenu"));
 
-    // Luego se la agregamos solo al que se presionó
-    nav.classList.remove("active"); //removemos el menú para cuando es clickeado
-    option.classList.add("activeMenu");
-  });
+  // Luego se la agregamos solo al que se presionó
+  nav.classList.remove("active");
 });
