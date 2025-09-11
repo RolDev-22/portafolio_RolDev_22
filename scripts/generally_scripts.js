42c3e0b1cca1057cd2d90d9dd335241d8d823f59
RolDev-22 <rolandomuagui1629@gmail.com>
2025-09-10 22:51:47 -0600

    Se agregan iamnenes paara fondo y de utilidad en la seccion contacto, además, se personaliza el form de contacto

--- scripts/generally_scripts.js
+++ scripts/generally_scripts.js
@@ -1,5 +1,7 @@
 const btnMenu = document.getElementById("hamburger");
 const optionsMenu = document.querySelectorAll(".optionsMenu");
+const inputsForm = document.querySelectorAll(".inputStyle");
+const labelsForm = document.querySelectorAll(".labelStyle");
 
 btnMenu.addEventListener("click", () => {
   const nav = document.querySelector("nav");
@@ -10,5 +12,19 @@ optionsMenu.forEach((option) => {
   option.addEventListener("click", () => {
     const nav = document.querySelector("nav");
     nav.classList.remove("active");
+  });
+});
+
+inputsForm.forEach((input, index) => {
+  input.addEventListener("focus", () => {
+    labelsForm[index].classList.add("activeLabel");
+  });
+  if (input.value != "") {
+    labelsForm[index].classList.add("activeLabel");
+  }
+  input.addEventListener("blur", () => {
+    if (input.value == "") {
+      labelsForm[index].classList.remove("activeLabel");
+    }
   });
 });
