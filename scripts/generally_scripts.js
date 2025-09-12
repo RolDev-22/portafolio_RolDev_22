38ee13d69734e997a1c187990ad3a95ff9a97aed
RolDev-22 <rolandomuagui1629@gmail.com>
2025-09-11 19:12:30 -0600

    Agregación de JQ UI para efectos visuales de entrada y salida, además se hacen ajustes en el css y html para mejora responsive general

--- scripts/generally_scripts.js
+++ scripts/generally_scripts.js
@@ -3,6 +3,18 @@ const optionsMenu = document.querySelectorAll(".optionsMenu");
 const inputsForm = document.querySelectorAll(".inputStyle");
 const labelsForm = document.querySelectorAll(".labelStyle");
 
+$(document).ready(function () {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      $(".JqLeft").hide("drop", { direction: "left" }, 2000);
+      $(".JqRight").hide("drop", { direction: "right" }, 2000);
+    } else {
+      $(".JqLeft").show("slide", { direction: "left" }, 2000);
+      $(".JqRight").show("slide", { direction: "right" }, 2000);
+    }
+  });
+});
+
 btnMenu.addEventListener("click", () => {
   const nav = document.querySelector("nav");
   nav.classList.toggle("active");
