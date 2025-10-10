1e160f555a453bb72a637d71332f5e37297beab2
RolDev-22 <rolandomuagui1629@gmail.com>
2025-10-10 02:35:24 -0600

    Cambios funcionales y de estillo

--- scripts/generally_scripts.js
+++ scripts/generally_scripts.js
@@ -2,15 +2,27 @@ const btnMenu = document.getElementById("burgerMenu");
 const nav = document.querySelector("nav");
 const links = document.querySelectorAll("nav ul li a");
 
+// Activa el menú al hacer clic
 links.forEach((link) => {
   link.addEventListener("click", activeMenu);
 });
 
 btnMenu.addEventListener("click", activeMenu);
 
-/* FUNCIONES */
-
-/* Esta función activa el menu de navegación en dispositivos móviles */
+// Esta función controla la visibilidad del menú dependiendo del tamaño de la pantalla
 function activeMenu() {
-  nav.classList.toggle("active");
+  if (window.innerWidth < 1024) {
+    nav.classList.toggle("active");
+  }
 }
+
+// Para asegurarse de que el menú esté correctamente visible cuando la pantalla se redimensione
+function checkMenuVisibility() {
+  if (window.innerWidth >= 1024) {
+    nav.classList.remove("active");
+  }
+}
+
+// Llamar a la función al cargar y cada vez que la ventana se redimensione
+window.addEventListener("resize", checkMenuVisibility);
+checkMenuVisibility();
