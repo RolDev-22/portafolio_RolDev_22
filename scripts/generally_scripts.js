bd83e8762410bc8443ff9269ad2c4f4c383abcd6
RolDev-22 <rolandomuagui1629@gmail.com>
2025-10-09 22:20:16 -0600

    Cambios varios de diseño y responsibe

--- scripts/generally_scripts.js
+++ scripts/generally_scripts.js
@@ -1,42 +1,16 @@
-const btnMenu = document.getElementById("hamburger");
-const optionsMenu = document.querySelectorAll(".optionsMenu");
-const inputsForm = document.querySelectorAll(".inputStyle");
-const labelsForm = document.querySelectorAll(".labelStyle");
+const btnMenu = document.getElementById("burgerMenu");
+const nav = document.querySelector("nav");
+const links = document.querySelectorAll("nav ul li a");
 
-$(document).ready(function () {
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 200) {
-      $(".JqLeft").hide("drop", { direction: "left" }, 2000);
-      $(".JqRight").hide("drop", { direction: "right" }, 2000);
-    } else {
-      $(".JqLeft").show("slide", { direction: "left" }, 2000);
-      $(".JqRight").show("slide", { direction: "right" }, 2000);
-    }
-  });
+links.forEach((link) => {
+  link.addEventListener("click", activeMenu);
 });
 
-btnMenu.addEventListener("click", () => {
-  const nav = document.querySelector("nav");
+btnMenu.addEventListener("click", activeMenu);
+
+/* FUNCIONES */
+
+/* Esta función activa el menu de navegación en dispositivos móviles */
+function activeMenu() {
   nav.classList.toggle("active");
-});
-
-optionsMenu.forEach((option) => {
-  option.addEventListener("click", () => {
-    const nav = document.querySelector("nav");
-    nav.classList.remove("active");
-  });
-});
-
-inputsForm.forEach((input, index) => {
-  input.addEventListener("focus", () => {
-    labelsForm[index].classList.add("activeLabel");
-  });
-  if (input.value != "") {
-    labelsForm[index].classList.add("activeLabel");
-  }
-  input.addEventListener("blur", () => {
-    if (input.value == "") {
-      labelsForm[index].classList.remove("activeLabel");
-    }
-  });
-});
+}
