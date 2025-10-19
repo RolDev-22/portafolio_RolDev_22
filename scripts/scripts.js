28945d01f2dbd8e2000fd81f3aa80262d2ea905c
RolDev-22 <rolandomuagui1629@gmail.com>
2025-10-19 12:40:25 -0600

    ajaustes en la sección contacto

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 const btnMenu = document.getElementById("burgerMenu");
 const nav = document.querySelector("nav");
 const links = document.querySelectorAll(".links-nav");
+const inputs = document.querySelectorAll(".inputControl");
 
 // Activa el menú al hacer clic
 links.forEach((link) => {
@@ -8,6 +9,26 @@ links.forEach((link) => {
 });
 
 btnMenu.addEventListener("click", activeMenu);
+
+// Controla la visibilidad de las etiquetas de los inputs
+inputs.forEach((input) => {
+  const labelInput = input.previousElementSibling;
+
+  // Verifica si el input ya tiene contenido al cargar la página
+  if (input.value.length > 0) {
+    labelInput.classList.add("activeInput");
+  }
+
+  input.addEventListener("focus", () => {
+    labelInput.classList.add("activeInput");
+  });
+
+  input.addEventListener("blur", () => {
+    if (input.value.length === 0) {
+      labelInput.classList.remove("activeInput");
+    }
+  });
+});
 
 // Esta función controla la visibilidad del menú dependiendo del tamaño de la pantalla
 function activeMenu() {
